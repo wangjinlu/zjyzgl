dff074176eb7624ca8ebb84579b4db8cf40a71be
沈先中 <google_1986@163.com>
2013-04-24 17:02:41 +0800

    Have a try!

---
+++ zjyzgl-web/src/main/java/com/wonders/zjyzgl/chartered/repository/CharteredRepository.java
@@ -0,0 +1,8 @@
+package com.wonders.zjyzgl.chartered.repository;
+
+import com.wonders.zjyzgl.chartered.entity.AdmissionCharteredInfo;
+import com.wonders.zjyzgl.core.repository.MyRepository;
+
+public interface CharteredRepository extends MyRepository<AdmissionCharteredInfo, String> {
+
+}
