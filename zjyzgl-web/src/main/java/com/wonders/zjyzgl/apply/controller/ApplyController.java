dc4e18fad6ada42afbdc34801696b104e107bc70
沈先中 <google_1986@163.com>
2013-04-28 17:35:21 +0800

    ff

--- zjyzgl-web/src/main/java/com/wonders/zjyzgl/apply/controller/ApplyController.java
+++ zjyzgl-web/src/main/java/com/wonders/zjyzgl/apply/controller/ApplyController.java
@@ -7,9 +7,12 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.wonders.zjyzgl.apply.entity.MatterApplyInfo;
 import com.wonders.zjyzgl.apply.repository.ApplyRepository;
+import com.wonders.zjyzgl.apply.service.ApplyService;
+import com.wonders.zjyzgl.chartered.entity.AdmissionCharteredInfo;
 import com.wonders.zjyzgl.core.controller.AbstractBaseController;
 import com.wonders.zjyzgl.core.repository.MyRepository;
 
@@ -23,10 +26,19 @@ public class ApplyController extends
 		AbstractBaseController<MatterApplyInfo, String> {
 	@Inject
 	private ApplyRepository applyRepository;
+	@Inject
+	private ApplyService applyService ;
 	@Override
 	public MyRepository<MatterApplyInfo, String> getRepository()
 	{
 		return this.applyRepository;
 	}
+	@RequestMapping(params ="method=save")
+	public String addApplyAndChartered(MatterApplyInfo matterApplyInfo,
+			AdmissionCharteredInfo admissionCharteredInfo)throws Exception
+	{
+		 this.applyService.addMatterApplyInfo(matterApplyInfo, admissionCharteredInfo);
+		 return "success";  
 
+	}
 }
--- zjyzgl-web/src/main/java/com/wonders/zjyzgl/apply/service/ApplyService.java
+++ zjyzgl-web/src/main/java/com/wonders/zjyzgl/apply/service/ApplyService.java
@@ -3,11 +3,28 @@ package com.wonders.zjyzgl.apply.service;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wonders.zjyzgl.apply.entity.MatterApplyInfo;
 import com.wonders.zjyzgl.apply.repository.ApplyRepository;
+import com.wonders.zjyzgl.chartered.entity.AdmissionCharteredInfo;
+import com.wonders.zjyzgl.chartered.repository.CharteredRepository;
 
 @Service
 public class ApplyService {
 	@Inject
 	private ApplyRepository applyRepository;
+	@Inject 
+	private CharteredRepository charteredRepository;
+	/**
+	 * 登记一条许可登记事项
+	 * @param matterApplyInfo 许可登记事项
+	 * @param admissionCharteredInfo  受理包车信息
+	 */
+	@Transactional
+	public void addMatterApplyInfo(MatterApplyInfo matterApplyInfo,AdmissionCharteredInfo admissionCharteredInfo)throws Exception
+	{
+		this.applyRepository.saveAndFlush(matterApplyInfo);
+		this.charteredRepository.saveAndFlush(admissionCharteredInfo);
+	}
 }
